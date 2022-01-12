cb39da03d02d13234e9df2edb36c92b750a8564f
ksy <ehsqjfwk99999@gmail.com>
2022-01-12 20:50:13 +0900

    4장 - 해시

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -59,7 +59,7 @@ var builtins = map[string]*object.Builtin{
 		arr := args[0].(*object.Array)
 		length := len(arr.Elements)
 		if length > 0 {
-			newElements := make([]object.Object, length-1, length-1)
+			newElements := make([]object.Object, length-1)
 			copy(newElements, arr.Elements[1:length])
 			return &object.Array{Elements: newElements}
 		}
@@ -77,7 +77,7 @@ var builtins = map[string]*object.Builtin{
 		arr := args[0].(*object.Array)
 		length := len(arr.Elements)
 
-		newElements := make([]object.Object, length+1, length+1)
+		newElements := make([]object.Object, length+1)
 		copy(newElements, arr.Elements)
 		newElements[length] = args[1]
 
